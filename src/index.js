9f3f615557977c77f88cd7d25f5aab2970e6fa50
Arhaan <arhaan.ahmad2003@gmail.com>
2020-08-30 21:45:55 +0530

    Ex 1.1 Corrected

--- src/index.js
+++ src/index.js
@@ -9,9 +9,17 @@ const Header = (props) => {
 
 const Course = (props) => {
   return(
-    <p>
-      {props.part} {props.exercise}
-    </p>
+    <>
+      <p>
+        {props.part1} {props.exercise1}
+      </p>
+      <p>
+        {props.part2} {props.exercise2}
+      </p>
+      <p>
+        {props.part3} {props.exercise3}
+      </p>
+    </>
   )
 }
 
@@ -33,9 +41,7 @@ const App = () => {
 
     <div>
       <Header course={course}/>
-      <Course part={part1} exercise={exercises1} />
-      <Course part={part2} exercise={exercises2} />
-      <Course part={part3} exercise={exercises3} />
+      <Course part1={part1} exercise1={exercises1} part2={part2} exercise2={exercises2} part3={part3} exercise3={exercises3} />
       <Total sum={exercises1 + exercises2 + exercises3}/>
     </div>
   )
