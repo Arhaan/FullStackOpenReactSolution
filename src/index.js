e7662372ac4cdf76775211cbd7355c112285b440
Arhaan <arhaan.ahmad2003@gmail.com>
2020-09-05 12:24:13 +0530

    1.8

--- src/index.js
+++ src/index.js
@@ -2,29 +2,57 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css';
 
+const Statistics = (props) => {
+  return(
+    <div>
+        <h1>Statistics</h1>
+
+        <p>Good {props.good}</p>
+        <p>Neutral {props.neutral}</p>
+        <p>Bad {props.bad}</p>
+        <p>All {props.all}</p>
+        <p>Average {props.average}</p>
+        <p>positive {props.positive} %</p>
+    </div>
+  )
+}
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
+  const updateAllAvgPositive = () =>{
+    setAll(all+1)
+    setAverage((good-bad)/all)
+    setPositive(good*100/all)
+  }
+  const increaseGood = () => {
+    setGood(good+1)
+    updateAllAvgPositive()
+  }
+  const increaseNeutral = () => {
+    setNeutral(neutral+1)
+    updateAllAvgPositive()
+  }
+  const increaseBad = () => {
+    setBad(bad+1)
+    updateAllAvgPositive()
+  }
   return (
     <div>
       <div>
         <h1>Give FeedBack</h1>
         <div>
-          <button onClick={()=>setGood(good+1)}>Good</button>
-          <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
-          <button onClick={()=>setBad(bad+1)}>Bad</button>
+          <button onClick={increaseGood}>Good</button>
+          <button onClick={increaseNeutral}>neutral</button>
+          <button onClick={increaseBad}>Bad</button>
         </div>
       </div>
+      <Statistics good={good} bad={bad} neutral={neutral} all={all} average={average} positive={positive}/>
 
-      <div>
-        <h1>Statistics</h1>
-
-        <p>Good {good}</p>
-        <p>Neutral {neutral}</p>
-        <p>Bad {bad}</p>
-      </div>
     </div>
   )
 }
