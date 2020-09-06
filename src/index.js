422a7aeed992c38d1b122ad258f856e2757d8edc
Arhaan <arhaan.ahmad2003@gmail.com>
2020-09-06 23:03:41 +0530

    2.3

--- src/index.js
+++ src/index.js
@@ -15,10 +15,12 @@ const Part = (props) => {
   )
 }
 const Course = (props) => {
+  const total = props.course.parts.reduce((s,p) =>s+p.exercises, 0)
   return(
     <>
       <Header course={props.course.name}/>
       {props.course.parts.map(part => <Part key={part.id} part = {part}/>)}
+      <Total sum={total}/>
     </>
   )
 }
