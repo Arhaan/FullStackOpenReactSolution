1ba069ac6fcf78c755e3aa4db6a5942f813e1d1f
Arhaan <arhaan.ahmad2003@gmail.com>
2020-09-06 23:11:14 +0530

    2.4

--- src/index.js
+++ src/index.js
@@ -27,35 +27,37 @@ const Course = (props) => {
 
 const Total = (props) => {
   return(
-    <p>Number of exercises {props.sum}</p>
+    <p><b>Total number of exercises {props.sum}</b></p>
   )
 }
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
+  const courses = [
       {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course}/>
+      {courses.map(course => <Course key={course.id} course={course}/>)}
     </div>
   )
 }
