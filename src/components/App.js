2bc0e4ff87a727a37bb7f7c01ea3b90f71205b22
Arhaan <arhaan.ahmad2003@gmail.com>
2020-09-13 17:57:29 +0530

    2.8

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,13 @@ import '../index.css'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      number: '0000000000'
+    }
   ])
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -14,14 +18,20 @@ const App = () => {
       return;
     }
     const newperson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     setPersons(persons.concat(newperson))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -30,13 +40,14 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+          Number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
